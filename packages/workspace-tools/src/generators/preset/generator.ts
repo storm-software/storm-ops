b3d08b6ae781436ae941669047bbbb62a95cc709
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2023-11-01 10:54:48 -0400

    feat(workspace-tools): Added initial code for tsup build executor

--- packages/workspace-tools/src/generators/preset/generator.ts
+++ packages/workspace-tools/src/generators/preset/generator.ts
@@ -21,10 +21,9 @@ export default async function (tree: Tree, options: PresetGeneratorSchema) {
   updateJson(tree, "package.json", json => {
     json.scripts = json.scripts || {};
 
-    json.name = "@storm-ops/monorepo";
     json.version = "0.0.0";
     json.private = true;
-    json.keywords = [
+    json.keywords ??= [
       "storm",
       "stormstack",
       "storm-stack",
@@ -43,14 +42,14 @@ export default async function (tree: Tree, options: PresetGeneratorSchema) {
       "monorepo"
     ];
 
-    json.homepage = "https://stormsoftware.org";
-    json.bugs = {
+    json.homepage ??= "https://stormsoftware.org";
+    json.bugs ??= {
       url: "https://stormsoftware.org/support",
       email: "support@stormsoftware.org"
     };
 
-    json.license = "Apache License 2.0";
-    json.author = {
+    json.license ??= "Apache License 2.0";
+    json.author ??= {
       name: "Storm Software",
       email: "contact@stormsoftware.org",
       url: "https://stormsoftware.org"
@@ -58,55 +57,48 @@ export default async function (tree: Tree, options: PresetGeneratorSchema) {
 
     // generate a start script into the package.json
 
-    json.scripts["affected:test"] =
-      "nx affected:test --parallel --maxParallel=5";
-    json.scripts["affected:e2e"] = "nx affected:e2e --parallel --maxParallel=5";
+    json.scripts.adr = "pnpm log4brains adr new";
+    json.scripts["adr-preview"] = "pnpm log4brains preview";
+    json.scripts.prepare = "pnpm prepare:husky && npx patch-package -y";
+    json.scripts["prepare:husky"] = "is-ci || husky install";
+    json.scripts.preinstall =
+      "pnpm @storm-software/workspace-tools/scripts/pre-install.js || npx -y only-allow pnpm";
+    json.scripts["install:csb"] =
+      "corepack enable && pnpm install --frozen-lockfile";
+
+    json.scripts.clean = "rimraf dist";
     json.scripts.prebuild = "pnpm clean";
     json.scripts.build = "nx affected -t build --parallel=5";
     json.scripts["build:all"] = "nx run-many -t build --all --parallel=5";
     json.scripts["build:production"] =
       "nx run-many -t build --all --prod --parallel=5";
-    json.scripts.clean = "rimraf dist";
-    json.scripts.commit = "storm-git commit";
-    json.scripts["dep-graph"] = "nx dep-graph";
-    json.scripts.e2e = "nx e2e";
-    json.scripts.format = "nx format:write && pnpm doctoc --github README.md";
-    json.scripts.help = "nx help";
-    json.scripts.preinstall =
-      "node -e \"if(process.env.CI == 'true') {console.log('Skipping preinstall...'); process.exit(1)}\" || npx -y only-allow pnpm";
-    json.scripts["install:csb"] =
-      "corepack enable && pnpm install --frozen-lockfile";
+
     json.scripts.nx = "nx";
     json.scripts.graph = "nx graph";
     json.scripts.lint = "pnpm storm-lint all";
-    json.scripts["pre-commit"] =
-      "lint-staged --concurrent false --config @storm-software/linting-tools/lint-staged/config";
-    json.scripts.prepare = "pnpm prepare:husky && npx patch-package -y";
-    json.scripts["prepare:husky"] = "is-ci || husky install";
-    json.scripts["repo-check:manypkg"] = "pnpm exec manypkg check";
     json.scripts.start = "nx serve";
     json.scripts.test = "nx test";
+    json.scripts.e2e = "nx e2e";
+    json.scripts.format = "nx format:write && pnpm doctoc --github README.md";
+    json.scripts.help = "nx help";
+    json.scripts["dep-graph"] = "nx dep-graph";
+    json.scripts["local-registry"] =
+      "nx local-registry @storm-software/storm-ops";
 
-    json.packageManager = "pnpm@8.7.4";
+    json.scripts["pre-push"] =
+      "lint-staged --concurrent false --config @storm-software/linting-tools/lint-staged/config";
+    json.scripts.commit = "pnpm storm-git commit";
+    json.scripts["api-extractor"] =
+      "nx g @storm-software/workspace-tools:api-extractor --outputPath 'docs/api-reference' --clean --no-interactive";
+    json.scripts.release = "pnpm storm-git release";
+
+    json.packageManager ??= "pnpm@8.7.4";
     json.engines = {
       node: ">=18.17.0",
       pnpm: ">=8.7.4"
     };
 
-    json.husky = {
-      hooks: {
-        "pre-commit":
-          "lint-staged --concurrent false --config @storm-software/linting-tools/lint-staged/config"
-      }
-    };
-
-    json.config = {
-      commitizen: {
-        path: "@commitlint/cz-commitlint"
-      }
-    };
-
-    json.prettier = "@storm-software/linting-tools/prettier";
+    json.prettier = "@storm-software/linting-tools/prettier/config";
     json.resolutions = {
       graphql: "^16.8.0",
       minimist: "^1.2.6"
@@ -146,16 +138,9 @@ export default async function (tree: Tree, options: PresetGeneratorSchema) {
       "@storm-software/git-tools": "latest",
       "@storm-software/linting-tools": "latest",
       "@storm-software/workspace-tools": "latest",
-      "@commitlint/config-nx-scopes": "latest",
-      "@commitlint/cli": "latest",
-      "@commitlint/config-conventional": "latest",
-      "@commitlint/cz-commitlint": "latest",
-      "@commitlint/prompt-cli": "latest",
-      "@manypkg/cli": "latest",
       "prettier-plugin-packagejson": "latest",
       "prettier-plugin-prisma": "latest",
       "patch-package": "latest",
-      doctoc: "latest",
       tsup: "latest",
       eslint: "latest",
       "eslint-config-next": "latest",
@@ -168,6 +153,7 @@ export default async function (tree: Tree, options: PresetGeneratorSchema) {
       "eslint-plugin-react-hooks": "latest",
       "eslint-plugin-storybook": "latest",
       "nx-cloud": "latest",
+      log4brains: "latest",
       husky: "latest",
       prettier: "latest",
       "is-ci": "latest",
