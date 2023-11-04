16fdc6768963d46bee0e6c0a2a4059afb27a12a1
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2023-11-04 11:52:08 -0400

    chore(git-tools): Added git hooks folder

--- packages/workspace-tools/scripts/pre-commit.js
+++
@@ -1,5 +0,0 @@
-const { execSync } = require("node:child_process");
-
-execSync(
-  'pnpm lint-staged --concurrent false --config="./lint-staged.config.js"'
-);
--- packages/workspace-tools/scripts/pre-push.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require("node:child_process");
-
-execSync("node ./packages/workspace-tools/scripts/check-lock-file.js");
-
-execSync(
-  "command -v git-lfs >/dev/null 2>&1 || { echo >&2 '\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/pre-push.\n'; exit 2; }"
-);
-execSync('git lfs pre-push "$@"');
