f5427a1314473ffb2598a6c9c88de73877778770
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2023-11-12 05:08:44 -0500

    fix(workspace-tools): Resolved export issue with tsup executor

--- packages/workspace-tools/src/executors/tsup/hasher.ts
+++ packages/workspace-tools/src/executors/tsup/hasher.ts
@@ -1,11 +1,9 @@
-import { CustomHasher } from "@nx/devkit";
-
 /**
  * This is a boilerplate custom hasher that matches
  * the default Nx hasher. If you need to extend the behavior,
  * you can consume workspace details from the context.
  */
-export const buildHasher: CustomHasher = async (task, context) => {
+export const buildHasher = async (task, context) => {
   return context.hasher.hashTask(task, context.taskGraph);
 };
 
