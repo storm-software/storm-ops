26f9ac996d9aadfbbbc9ca245406d2fd9bd3b9b5
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-01-15 03:46:17 -0500

    fix(git-tools): Resolved issue with Auto-approval GitHub Action

--- infrastructure/index.ts
+++
@@ -1,7 +0,0 @@
-import * as github from "@pulumi/github";
-
-const repo = new github.Repository("demo-repo", {
-  description: "Demo Repository for GitHub"
-});
-
-export const clusterName = repo.name;
