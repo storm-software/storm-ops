bef2780c6be49adf01c5aeda9b239762c3c9366e
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-02-09 18:11:29 +0000

    fix(config-tools): Update `run` function to use a nodejs child process

--- packages/config-tools/src/utilities/run.ts
+++ packages/config-tools/src/utilities/run.ts
@@ -1,5 +1,5 @@
 import type { StormConfig } from "@storm-software/config";
-import { execaCommandSync } from "execa";
+import { execSync } from "node:child_process";
 
 export const LARGE_BUFFER = 1024 * 1000000;
 
@@ -7,28 +7,22 @@ export const LARGE_BUFFER = 1024 * 1000000;
  *  Run a command line process
  *
  * @remarks
- * A wrapper around execa to run our command line processes
+ * A wrapper around execSync to run our command line processes
  *
  * @param config - The Storm configuration object
  * @param command - The command to run
  * @param cwd - The current working directory
  * @returns The result of the command
  */
-export const run = (
-  config: StormConfig,
-  command: string,
-  cwd: string = config.workspaceRoot,
-) => {
-  return execaCommandSync(command, {
-    preferLocal: true,
-    shell: true,
+export const run = (config: StormConfig, command: string, cwd: string = config.workspaceRoot) => {
+  return execSync(command, {
     cwd,
     env: {
       ...process.env,
-      FORCE_COLOR: "true",
+      FORCE_COLOR: "true"
     },
     stdio: "inherit",
     maxBuffer: LARGE_BUFFER,
-    killSignal: "SIGTERM",
+    killSignal: "SIGTERM"
   });
 };
