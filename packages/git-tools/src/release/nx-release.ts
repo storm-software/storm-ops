415c26818eeb009e48405184b8c8b170404cbb18
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-02-13 15:58:22 +0000

    fix(git-tools): Fixes to the Storm-Git release process

--- packages/git-tools/src/release/nx-release.ts
+++ packages/git-tools/src/release/nx-release.ts
@@ -1,11 +1,24 @@
 import type { StormConfig } from "@storm-software/config";
-import { writeDebug, writeInfo, writeSuccess } from "@storm-software/config-tools";
+import {
+  writeDebug,
+  writeError,
+  writeInfo,
+  writeSuccess,
+  writeWarning
+} from "@storm-software/config-tools";
 import { createNxReleaseConfig } from "nx/src/command-line/release/config/config.js";
+import { filterReleaseGroups } from "nx/src/command-line/release/config/filter-release-groups";
 import {
   releaseChangelog,
   releasePublish,
   releaseVersion
 } from "nx/src/command-line/release/index.js";
+import { gitCommit, gitTag } from "nx/src/command-line/release/utils/git";
+import {
+  createCommitMessageValues,
+  createGitTagValues,
+  handleDuplicateGitTags
+} from "nx/src/command-line/release/utils/shared";
 import { readNxJson } from "nx/src/config/nx-json.js";
 import { createProjectGraphAsync } from "nx/src/project-graph/project-graph.js";
 
@@ -42,8 +55,7 @@ export const runRelease = async (
   writeDebug(config, "Calling `createNxReleaseConfig` to determine the release configuration...");
   const { error: configError, nxReleaseConfig } = await createNxReleaseConfig(
     projectGraph,
-    nxJson.release,
-    "nx-release-publish"
+    nxJson.release
   );
   if (configError) {
     throw new Error(
@@ -53,12 +65,17 @@ export const runRelease = async (
 
   writeInfo(config, "Determining the current release version...");
 
+  const shouldCommit = nxJson.release?.git?.commit ?? true;
+  const shouldStage = (shouldCommit || nxJson.release?.git?.stageChanges) ?? true;
+  const shouldTag = nxJson.release?.git?.tag ?? true;
+
   const { workspaceVersion, projectsVersionData } = await releaseVersion({
     dryRun: !!options.dryRun,
     verbose: true,
     preid: config.preid,
-    stageChanges: true,
-    gitCommit: false
+    stageChanges: shouldStage,
+    gitCommit: false,
+    gitTag: false
   });
 
   writeInfo(config, "Generating the release changelog files...");
@@ -70,20 +87,79 @@ export const runRelease = async (
     verbose: true,
     to: options.head ?? process.env.NX_HEAD,
     from: options.base ?? process.env.NX_BASE,
-    gitRemote: "origin",
-    gitCommit: true,
-    gitCommitMessage: `chore(${
-      options.project ? options.project : "monorepo"
-    }): Release latest workspace packages [skip ci]`,
-    workspaceChangelog: nxReleaseConfig.projectsRelationship === "fixed"
+    stageChanges: shouldStage,
+    gitCommit: false,
+    gitTag: false
   });
 
-  writeInfo(config, "Publishing the release...");
+  const {
+    error: filterError,
+    releaseGroups,
+    releaseGroupToFilteredProjects
+  } = filterReleaseGroups(projectGraph, nxReleaseConfig);
+  if (filterError) {
+    writeError(config, "An error occurred filtering the release groups");
+    throw new Error(`${filterError.title}: \n${filterError.bodyLines.join("\n")}`);
+  }
 
-  await releasePublish({
-    dryRun: !!options.dryRun,
-    verbose: true
-  });
+  if (shouldCommit) {
+    writeInfo(config, "Committing changes with git");
+
+    const commitMessageValues: string[] = createCommitMessageValues(
+      releaseGroups,
+      releaseGroupToFilteredProjects,
+      projectsVersionData,
+      "chore({projectName}): Release version {version} [skip ci]"
+    );
+
+    await gitCommit({
+      messages: commitMessageValues,
+      additionalArgs: nxReleaseConfig.git.commitArgs,
+      dryRun: false,
+      verbose: true
+    });
+  }
+
+  if (shouldTag) {
+    writeInfo(config, "Tagging commit with git");
+
+    // Resolve any git tags as early as possible so that we can hard error in case of any duplicates before reaching the actual git command
+    const gitTagValues: string[] = createGitTagValues(
+      releaseGroups,
+      releaseGroupToFilteredProjects,
+      projectsVersionData
+    );
+    handleDuplicateGitTags(gitTagValues);
+
+    for (const tag of gitTagValues) {
+      await gitTag({
+        tag,
+        message: nxReleaseConfig.git.tagMessage,
+        additionalArgs: nxReleaseConfig.git.tagArgs,
+        dryRun: false,
+        verbose: true
+      });
+    }
+  }
+
+  let hasNewVersion = false;
+
+  // null means that all projects are versioned together but there were no changes
+  if (workspaceVersion !== null) {
+    hasNewVersion = Object.values(projectsVersionData).some(
+      (version) => version.newVersion !== null
+    );
+  }
+
+  if (hasNewVersion) {
+    writeInfo(config, "Publishing the release...");
+    await releasePublish({
+      dryRun: !!options.dryRun,
+      verbose: true
+    });
+  } else {
+    writeWarning(config, "Skipped publishing packages.");
+  }
 
   writeSuccess(config, "Completed the release process!");
 };
