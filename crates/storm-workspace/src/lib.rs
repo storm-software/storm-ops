42e5a411d8a6813ed47984b20207276c2b92831c
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-03-25 12:49:11 +0100

    fix(storm-workspace): Resolved issue with failed unit-test

--- crates/storm-workspace/src/lib.rs
+++ crates/storm-workspace/src/lib.rs
@@ -1,20 +1,19 @@
-pub mod utils;
 pub mod errors;
+pub mod utils;
 
 #[cfg(test)]
 mod tests {
-    use crate::utils::get_workspace_root;
-    use std::fs::read_to_string;
+  use crate::utils::get_workspace_root;
+  use std::fs::read_to_string;
 
-    #[test]
-    fn it_should_find_workspace_root() {
-        let crate_name = "name = \"project-root\"";
+  #[test]
+  fn it_should_find_workspace_root() {
+    let crate_name = "name = 'storm-ops'";
 
-        let project_root = get_workspace_root().expect("There is no project root");
+    let project_root = get_workspace_root().expect("There is no project root");
+    let toml_path = project_root.to_str().unwrap().to_owned() + "/Cargo.toml";
+    let toml_string = read_to_string(toml_path).unwrap();
 
-        let toml_path = project_root.to_str().unwrap().to_owned() + "/Cargo.toml";
-        let toml_string = read_to_string(toml_path).unwrap();
-
-        assert!(toml_string.contains(crate_name));
-    }
+    assert!(toml_string.contains(crate_name));
+  }
 }
