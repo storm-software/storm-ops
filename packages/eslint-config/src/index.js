d9a3f3836e385073790af009b0062111074ea996
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-07-14 10:30:05 +0100

    feat(eslint-config): Added perfectionist plugins to improve base configuration

--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -1,5 +1,16 @@
 module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
   extends: [
+    "eslint:recommended",
+    "plugin:perfectionist/recommended-natural",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:regexp/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "./rules/best-practices",
     "./rules/errors",
     "./rules/node",
@@ -9,11 +20,87 @@ module.exports = {
     "./rules/imports",
     "./rules/strict",
     "./rules/react",
-    "./rules/react-a11y"
+    "./rules/react-a11y",
+    "prettier"
   ].map(require.resolve),
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
     sourceType: "module"
   },
-  rules: {}
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.js", "*.cjs"],
+      extends: ["plugin:@typescript-eslint/disable-type-checked"]
+    }
+  ],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/consistent-type-imports": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+
+    // Type-aware any rules:
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    // This rule doesn't work well in .tsx files
+    "@typescript-eslint/no-misused-promises": "off",
+    // Type-aware any rules end
+
+    // ts-expect should always be preferred over ts-ignore
+    "@typescript-eslint/prefer-ts-expect-error": "warn",
+
+    // This rule makes no sense when overriding class methods. This is used a lot in richtext-lexical.
+    "class-methods-use-this": "off",
+
+    // By default, it errors for unused variables. This is annoying, warnings are enough.
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: false,
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^ignore"
+      }
+    ],
+
+    "@typescript-eslint/no-use-before-define": "off",
+    "arrow-body-style": 0,
+    "import/prefer-default-export": "off",
+    "no-console": "warn",
+    "no-sparse-arrays": "off",
+    "no-underscore-dangle": "off",
+    "no-use-before-define": "off",
+    "object-shorthand": "warn",
+    "react/no-unused-prop-types": "off",
+    "react/prop-types": "off",
+    "react/require-default-props": "off",
+
+    "perfectionist/sort-objects": [
+      "error",
+      {
+        type: "natural",
+        order: "asc",
+        "partition-by-comment": true,
+        groups: ["top", "unknown"],
+        "custom-groups": {
+          top: ["_id", "id", "name", "slug", "type"]
+        }
+      }
+    ]
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    }
+  }
 };
