7bca49cefe6333093997cf613ab03b4b6866c3c0
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-07-31 15:18:48 +0100

    chore(storm-ops): Update base eslint configuration to use local plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
-import storm from "./dist/packages/eslint/dist/preset.mjs";
+import pluginPlugin from "eslint-plugin-eslint-plugin";
+import stormPlugin from "./dist/plugins/eslint/index.mjs";
 
-export default storm({
-  typescript: {
-    "unicorn/no-null": 0,
-    "react/require-default-props": 0,
-    "unicorn/no-useless-switch-case": 0,
-    "react/jsx-closing-bracket-location": 0,
-    "no-undef": 0,
-    "unicorn/consistent-function-scoping": 0,
-    "class-methods-use-this": 0
-  },
-  ignores: ["**/dist", "**/node_modules", "**/.nx"]
-});
+export default [
+  ...stormPlugin.configs.recommended,
+  {
+    ...pluginPlugin.configs.recommended,
+    plugins: {
+      "eslint-plugin": pluginPlugin
+    },
+    files: ["packages/{eslint-plugin,eslint-plugin-*}/**/*"]
+  }
+];
