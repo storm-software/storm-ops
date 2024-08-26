4ce6ac9ea8df2f3b99575f0921a188106b8f7569
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-08-26 13:48:38 +0100

    deps(storm-ops): Upgrade the workspace's Nx version

--- packages/workspace-tools/src/generators/init/init.ts
+++ packages/workspace-tools/src/generators/init/init.ts
@@ -9,9 +9,9 @@ export async function initGenerator(tree: Tree, schema: Schema) {
   const task = addDependenciesToPackageJson(
     tree,
     {
-      nx: "^19.5.6",
-      "@nx/workspace": "^19.5.6",
-      "@nx/js": "^19.5.6",
+      nx: "^19.6.2",
+      "@nx/workspace": "^19.6.2",
+      "@nx/js": "^19.6.2",
       "@storm-software/eslint": "latest",
       "@storm-software/prettier": "latest",
       "@storm-software/config-tools": "latest",
