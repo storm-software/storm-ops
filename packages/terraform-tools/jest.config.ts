16c88699619490a3b2ce965546721b8c44cad706
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-08-26 11:53:47 +0100

    fix(terraform-tools): Resolved issue with the Jest configuration file

--- packages/terraform-tools/jest.config.ts
+++ packages/terraform-tools/jest.config.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: 'terraform-tools',
+  displayName: "terraform-tools",
   preset: "../../jest.preset.js",
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }]
   },
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageDirectory: "../../coverage/packages/terraform-tools'
+  coverageDirectory: "../../coverage/packages/terraform-tools"
 };
