c96e9a9ee57fca07da55323695ea169b2e737dea
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-08-31 01:08:40 +0100

    fix(terraform-modules): Resolved issue with conditional statement syntax

--- terraform-modules/aws/api-gateway/locals.tf
+++ terraform-modules/aws/api-gateway/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  full_name = var.full_name ? var.full_name : "${var.environment}_${var.region}_${var.name}"
+  full_name = var.full_name != null ? var.full_name : "${var.environment}_${var.region}_${var.name}"
 }
