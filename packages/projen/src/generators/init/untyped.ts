94a9f01ab50a940f7e7e5389633abd883c673305
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-01-29 14:24:02 +0000

    fix(projen): Resolve build issue with init generator schema

--- packages/projen/src/generators/init/untyped.ts
+++ packages/projen/src/generators/init/untyped.ts
@@ -1,10 +1,19 @@
+import baseGeneratorSchema from "@storm-software/workspace-tools/base/base-generator.untyped";
 import { defineUntypedSchema } from "untyped";
 
 export default defineUntypedSchema({
+  ...baseGeneratorSchema,
   $schema: {
     id: "InitGeneratorSchema",
     title: "Init Generator",
-    description: "A type definition for an init generator schema"
+    description: "A type definition for the Projen init generator schema"
+  },
+  directory: {
+    $schema: {
+      title: "Directory",
+      type: "string",
+      description: "The directory to initialize the workspace in"
+    }
   },
   skipFormat: {
     $schema: {
