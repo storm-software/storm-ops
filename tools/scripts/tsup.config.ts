9b6a4a1ca7e575e4663cdb84ef2f38a1b6ea4a99
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-02-03 12:53:07 +0000

    feat(workspace-tools): Added various new linting tasks and Nx plugin configuration

--- tools/scripts/tsup.config.ts
+++ tools/scripts/tsup.config.ts
@@ -8,10 +8,16 @@ export default defineConfig({
   format: ["cjs", "esm"],
   bundle: true,
   splitting: true,
+  treeshake: true,
+  keepNames: true,
   clean: true,
   sourcemap: false,
   tsconfig: "./tsconfig.json",
   dts: {
     resolve: true
+  },
+  onSuccess: async () => {
+    // eslint-disable-next-line no-console
+    console.log("tools-scripts build completed successfully!");
   }
 });
