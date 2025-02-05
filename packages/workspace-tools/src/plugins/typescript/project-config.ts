da504b0347460bd1467f28b078e4ba4b1d1d9082
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-02-05 03:51:20 +0000

    chore(markdownlint): Update config files to use `.jsonc`

--- packages/workspace-tools/src/plugins/typescript/project-config.ts
+++ packages/workspace-tools/src/plugins/typescript/project-config.ts
@@ -74,7 +74,7 @@ export const createNodes: CreateNodes<TypeScriptPluginOptions> = [
         executor: "nx:run-commands",
         options: {
           command:
-            'pnpm exec markdownlint-cli2 "{projectRoot}/**/*.{md,mdx,markdown}" "#**/node_modules/**/*" --config "node_modules/@storm-software/markdownlint/config/base.markdownlint-cli2.json" --fix'
+            'pnpm exec markdownlint-cli2 "{projectRoot}/**/*.{md,mdx,markdown}" "#**/node_modules/**/*" --config "node_modules/@storm-software/markdownlint/config/base.markdownlint-cli2.jsonc" --fix'
         }
       };
     }
