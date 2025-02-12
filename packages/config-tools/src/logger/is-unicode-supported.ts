7681f4031b7a5217616365f56c7ced886b1774fc
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-02-12 10:46:10 +0000

    fix(config-tools): Resolve issue with invalid access to env values

--- packages/config-tools/src/logger/is-unicode-supported.ts
+++ packages/config-tools/src/logger/is-unicode-supported.ts
@@ -1,23 +1,19 @@
-import process from "node:process";
 
 export function isUnicodeSupported() {
-  const { env } = process;
-  const { TERM, TERM_PROGRAM } = env;
-
   if (process.platform !== "win32") {
-    return TERM !== "linux"; // Linux console (kernel)
+    return process.env.TERM !== "linux"; // Linux console (kernel)
   }
 
   return (
-    Boolean(env.WT_SESSION) || // Windows Terminal
-    Boolean(env.TERMINUS_SUBLIME) || // Terminus (<0.2.27)
-    env.ConEmuTask === "{cmd::Cmder}" || // ConEmu and cmder
-    TERM_PROGRAM === "Terminus-Sublime" ||
-    TERM_PROGRAM === "vscode" ||
-    TERM === "xterm-256color" ||
-    TERM === "alacritty" ||
-    TERM === "rxvt-unicode" ||
-    TERM === "rxvt-unicode-256color" ||
-    env.TERMINAL_EMULATOR === "JetBrains-JediTerm"
+    Boolean(process.env.WT_SESSION) || // Windows Terminal
+    Boolean(process.env.TERMINUS_SUBLIME) || // Terminus (<0.2.27)
+    process.env.ConEmuTask === "{cmd::Cmder}" || // ConEmu and cmder
+    process.env.TERM_PROGRAM === "Terminus-Sublime" ||
+    process.env.TERM_PROGRAM === "vscode" ||
+    process.env.TERM === "xterm-256color" ||
+    process.env.TERM === "alacritty" ||
+    process.env.TERM === "rxvt-unicode" ||
+    process.env.TERM === "rxvt-unicode-256color" ||
+    process.env.TERMINAL_EMULATOR === "JetBrains-JediTerm"
   );
 }
