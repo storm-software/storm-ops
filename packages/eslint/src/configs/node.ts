d3a18d95428965aa9e81aae28eb4ec6140cc8753
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-02-23 14:48:26 +0000

    fix(eslint): Set `node/prefer-global/process` rule to `always`

--- packages/eslint/src/configs/node.ts
+++ packages/eslint/src/configs/node.ts
@@ -15,7 +15,7 @@ export async function node(): Promise<TypedFlatConfigItem[]> {
         "node/no-new-require": "error",
         "node/no-path-concat": "error",
         "node/prefer-global/buffer": ["error", "never"],
-        "node/prefer-global/process": ["error", "never"],
+        "node/prefer-global/process": ["error", "always"],
         "node/process-exit-as-throw": "error"
       }
     }
