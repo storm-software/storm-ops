4a11c8907602344d5542a3c33f8a3eff2192033e
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2025-03-16 03:44:34 +0100

    chore(git-tools): Do not split changelog-renderer code

--- packages/git-tools/tsup.changelog-renderer.ts
+++ packages/git-tools/tsup.changelog-renderer.ts
@@ -8,6 +8,7 @@ export default defineConfig([
     format: ["cjs", "esm"],
     outDir: "dist/changelog-renderer",
     platform: "node",
+    splitting: false,
     bundle: true,
     shims: true,
     clean: true,
