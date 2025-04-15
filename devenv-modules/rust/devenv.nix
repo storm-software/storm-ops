29e61695a9f358141d5d0412d97727a8ce943a09
Pat <pat@patsullivan.org>
2025-04-15 03:07:54 +0000

    feat(devenv-modules): Initial check-in of `devenv` modules

---
+++ devenv-modules/rust/devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, inputs, ... }:
+{
+  name = "storm-software_rust";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.cargo-deny
+  ];
+
+  # https://devenv.sh/languages/
+  languages.rust = {
+    enable = true;
+    channel = "nightly";
+    toolchain = inputs.nixpkgs;
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+    ];
+  };
+
+  # https://devenv.sh/processes/
+  processes.cargo-watch.exec = "cargo-watch";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
+
