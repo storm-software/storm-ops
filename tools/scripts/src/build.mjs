f03eda79f23716d7529322fe8d4833381bb51d2d
Pat <pat@patsullivan.org>
2025-04-17 20:53:25 +0000

    chore(monorepo): Added `build`, and `nuke` scripts

---
+++ tools/scripts/src/build.mjs
@@ -0,0 +1,63 @@
+#!/usr/bin/env zx
+
+import { $, argv, chalk, echo } from "zx";
+
+// usePwsh();
+
+try {
+  let configuration = argv.configuration;
+  if (!configuration) {
+    if (argv.prod) {
+      configuration = "production";
+    } else if (argv.dev) {
+      configuration = "development";
+    } else {
+      configuration = "production";
+    }
+  }
+
+  echo`${chalk.whiteBright(`📦  Building the monorepo in ${configuration} mode...`)}`;
+
+  let proc = $`pnpm bootstrap`.timeout("60s");
+  proc.stdout.on("data", data => {
+    echo`${data}`;
+  });
+  let result = await proc;
+  if (!result.ok) {
+    throw new Error(
+      `An error occurred while bootstrapping the monorepo: \n\n${result.message}\n`
+    );
+  }
+
+  if (configuration === "production") {
+    proc = $`pnpm nx run-many --target=build --all --exclude="@storm-stack/monorepo" --configuration=production --parallel=5`;
+    proc.stdout.on("data", data => {
+      echo`${data}`;
+    });
+    result = await proc;
+
+    if (!result.ok) {
+      throw new Error(
+        `An error occurred while building the monorepo in production mode: \n\n${result.message}\n`
+      );
+    }
+  } else {
+    proc = $`pnpm nx run-many --target=build --all --exclude="@storm-stack/monorepo" --configuration=${configuration} --nxBail`;
+    proc.stdout.on("data", data => {
+      echo`${data}`;
+    });
+    result = await proc;
+
+    if (!result.ok) {
+      throw new Error(
+        `An error occurred while building the monorepo in development mode: \n\n${result.message}\n`
+      );
+    }
+  }
+
+  echo`${chalk.green(`Successfully built the monorepo in ${configuration} mode!`)}`;
+} catch (error) {
+  echo`${chalk.red(error?.message ? error.message : "A failure occurred while building the monorepo")}`;
+
+  process.exit(1);
+}
---
+++ tools/scripts/src/nuke.mjs
@@ -0,0 +1,70 @@
+#!/usr/bin/env zx
+
+import { $, chalk, echo } from "zx";
+
+// usePwsh();
+
+try {
+  echo`${chalk.whiteBright("💣  Nuking the monorepo...")}`;
+
+  let proc = $`pnpm nx clear-cache`.timeout(`${5 * 60}s`);
+  proc.stdout.on("data", data => {
+    echo`${data}`;
+  });
+  let result = await proc;
+  if (!result.ok) {
+    throw new Error(
+      `An error occurred while clearing Nx cache: \n\n${result.message}\n`
+    );
+  }
+
+  proc = $`rm -rf ./.nx/cache ./.nx/workspace-data ./dist ./tmp`.timeout(
+    `${5 * 60}s`
+  );
+  proc.stdout.on("data", data => {
+    echo`${data}`;
+  });
+  result = await proc;
+  if (!result.ok) {
+    throw new Error(
+      `An error occurred while removing cache directories: \n\n${result.message}\n`
+    );
+  }
+
+  proc = $`rm -rf ./packages/*/node_modules`.timeout(`${5 * 60}s`);
+  proc.stdout.on("data", data => {
+    echo`${data}`;
+  });
+  result = await proc;
+  if (!result.ok) {
+    throw new Error(
+      `An error occurred while removing node modules and build directories from the monorepo's projects: \n\n${result.message}\n`
+    );
+  }
+
+  proc = $`rm -rf ./tools/*/node_modules`.timeout(`${5 * 60}s`);
+  proc.stdout.on("data", data => {
+    echo`${data}`;
+  });
+  result = await proc;
+  if (!result.ok) {
+    throw new Error(
+      `An error occurred while removing node modules and build directories from the monorepo's projects: \n\n${result.message}\n`
+    );
+  }
+
+  proc = $`rm -rf ./node_modules`.timeout(`${5 * 60}s`);
+  proc.stdout.on("data", data => {
+    echo`${data}`;
+  });
+  result = await proc;
+  if (!result.ok) {
+    throw new Error(
+      `An error occurred while removing node modules and build directories from the monorepo's projects: \n\n${result.message}\n`
+    );
+  }
+
+  echo`${chalk.green("Successfully nuked the cache, node modules, and build folders \n\n")}`;
+} catch (error) {
+  echo`${chalk.red(error?.message ? error.message : "A failure occurred while nuking the monorepo")}`;
+}
