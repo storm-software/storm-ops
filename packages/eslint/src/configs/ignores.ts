debd59a147f86f38f4b59355b1fc56fc71c384d0
Pat <pat@patsullivan.org>
2025-04-28 22:39:43 +0000

    fix(eslint): Resolve issue with provided ignores list

--- packages/eslint/src/configs/ignores.ts
+++ packages/eslint/src/configs/ignores.ts
@@ -1,14 +1,12 @@
-import defu from "defu";
 import type { TypedFlatConfigItem } from "../types";
 import { GLOB_EXCLUDE } from "../utils/constants";
-import { DEFAULT_IGNORES } from "../utils/ignores";
 
 export async function ignores(
   userIgnores: string[] = []
 ): Promise<TypedFlatConfigItem[]> {
   return [
     {
-      ignores: defu(GLOB_EXCLUDE, DEFAULT_IGNORES, userIgnores),
+      ignores: [...GLOB_EXCLUDE, ...userIgnores],
       name: "storm/ignores"
     }
   ];
