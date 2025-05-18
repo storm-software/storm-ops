56205028e66c437a74faea76429cffa5a4d88352
Pat <pat@patsullivan.org>
2025-05-18 08:59:45 +0000

    chore(monorepo): Add back dotenv configuration to devenv file

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 {
   name = "storm-software/storm-ops";
+
+  dotenv.enable = true;
+  dotenv.filename = [".env" ".env.local"];
+  dotenv.disableHint = true;
+
+  # https://devenv.sh/basics/
+  env.DEFAULT_LOCALE = "en_US";
+  env.DEFAULT_TIMEZONE = "America/New_York";
 }
 
