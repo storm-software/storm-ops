b027e61763900a94051ac4dbbd2be7adf39de790
Pat <pat@patsullivan.org>
2025-05-18 10:50:20 +0000

    feat(monorepo): Added the shared configuration project

---
+++ tools/config/README.md
@@ -0,0 +1,4 @@
+# Storm-Ops - Shared Config Tools
+
+A collection of tools for managing shared configuration files used in the
+Storm-Ops monorepo.
---
+++ tools/config/tsup.shared.ts
@@ -0,0 +1,27 @@
+import type { Options } from "tsup";
+import { defineConfig } from "tsup";
+
+export const getTsupConfig = (
+  options: Partial<Options> & Pick<Options, "name" | "entryPoints">
+) =>
+  defineConfig({
+    target: "node22",
+    outDir: "dist",
+    format: ["cjs", "esm"],
+    bundle: true,
+    splitting: true,
+    treeshake: true,
+    keepNames: true,
+    clean: true,
+    sourcemap: false,
+    platform: "node",
+    tsconfig: "./tsconfig.json",
+    dts: {
+      resolve: true
+    },
+    onSuccess: async () => {
+      // eslint-disable-next-line no-console
+      console.log(`${options.name} build completed successfully!`);
+    },
+    ...options
+  });
