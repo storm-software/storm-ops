5cb8169ee1312a1a2a005e0f6da84e674852ae32
Pat <pat@patsullivan.org>
2025-05-18 10:29:12 +0000

    fix(monorepo): Resolve issue with `release` step in CI workflow

--- tools/scripts/src/bootstrap.mjs
+++ tools/scripts/src/bootstrap.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env zx
 
 import { build } from "esbuild";
-import { $, chalk, echo } from "zx";
+import { chalk, echo } from "zx";
 
 try {
   echo`${chalk.whiteBright("⚙️  Bootstrapping the monorepo...")}`;
@@ -47,16 +47,16 @@ try {
     echo`${chalk.green("All Storm plugin built successfully")}`;
   });
 
-  const proc = $`pnpm nx reset --onlyDaemon`.timeout(`${5 * 60}s`);
-  proc.stdout.on("data", data => {
-    echo`${data}`;
-  });
-  const result = await proc;
-  if (!result.ok) {
-    throw new Error(
-      `An error occurred while resetting the Nx daemon process: \n\n${result.message}\n`
-    );
-  }
+  // const proc = $`pnpm nx reset --onlyDaemon`.timeout(`${5 * 60}s`);
+  // proc.stdout.on("data", data => {
+  //   echo`${data}`;
+  // });
+  // const result = await proc;
+  // if (!result.ok) {
+  //   throw new Error(
+  //     `An error occurred while resetting the Nx daemon process: \n\n${result.message}\n`
+  //   );
+  // }
 
   echo`${chalk.green("Completed monorepo bootstrapping successfully!")}`;
 } catch (error) {
