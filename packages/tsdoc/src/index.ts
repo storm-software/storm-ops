f70c330a7cf82333ccc34a6e3bcf0022c54507c5
Pat <pat@patsullivan.org>
2025-06-05 00:58:47 +0000

    chore(tsdoc): Rename `config.json` to `core.json`

--- packages/tsdoc/src/index.ts
+++ packages/tsdoc/src/index.ts
@@ -1,7 +1,7 @@
 import apiExtractor from "./api-extractor.json";
 import base from "./base.json";
-import config from "./config.json";
+import core from "./core.json";
 import typedoc from "./typedoc.json";
 
-export const configs = { base, typedoc, apiExtractor, config };
+export const configs = { base, typedoc, apiExtractor, core };
 export default configs;
