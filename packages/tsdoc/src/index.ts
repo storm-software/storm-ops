f20719498de6c73879e972e5e47fea0340d21aac
Pat <pat@patsullivan.org>
2025-06-05 00:51:09 +0000

    feat(tsdoc): Added the `api-extractor.json` and `config.json` files

--- packages/tsdoc/src/index.ts
+++ packages/tsdoc/src/index.ts
@@ -1,5 +1,7 @@
+import apiExtractor from "./api-extractor.json";
 import base from "./base.json";
+import config from "./config.json";
 import typedoc from "./typedoc.json";
 
-export const configs = { base, typedoc };
+export const configs = { base, typedoc, apiExtractor, config };
 export default configs;
