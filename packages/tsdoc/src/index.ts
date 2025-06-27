2f70b47a309eb918d061630b61506ea00f5bbfe6
Pat <pat@patsullivan.org>
2025-06-27 14:10:45 +0000

    feat(tsdoc): Added the `callouts` and `recommended` configuration JSONs

--- packages/tsdoc/src/index.ts
+++ packages/tsdoc/src/index.ts
@@ -1,7 +1,17 @@
 import apiExtractor from "./api-extractor.json";
 import base from "./base.json";
+import callouts from "./callouts.json";
 import core from "./core.json";
+import recommended from "./recommended.json";
 import typedoc from "./typedoc.json";
 
-export const configs = { base, typedoc, apiExtractor, core };
+export const configs = {
+  base,
+  typedoc,
+  apiExtractor,
+  core,
+  callouts,
+  recommended
+} as const;
+
 export default configs;
