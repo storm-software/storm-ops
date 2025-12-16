5273afce34261c7572679e208d7869cffeea04bb
Pat <pat@patsullivan.org>
2025-12-16 03:31:55 +0000

    chore(cloudflare-tools): Add limit to stream concurrency when uploading to R2 buckets

--- packages/cloudflare-tools/src/utils/http-handler.ts
+++ packages/cloudflare-tools/src/utils/http-handler.ts
@@ -6,8 +6,9 @@ import { NodeHttp2Handler } from "@smithy/node-http-handler";
  * @returns A NodeHttp2Handler instance with custom request and session timeouts
  */
 export function createHttpHandler() {
-  return new NodeHttp2Handler({
+  return NodeHttp2Handler.create({
     requestTimeout: 20 * 60 * 1000, // 20 minutes
-    sessionTimeout: 30 * 60 * 1000 // 30 minutes
+    sessionTimeout: 30 * 60 * 1000, // 30 minutes
+    maxConcurrentStreams: 100
   });
 }
