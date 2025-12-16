4d68a405e8c03d05f930c9720754ca826b8f69a1
Pat <pat@patsullivan.org>
2025-12-16 04:03:47 +0000

    fix(cloudflare-tools): Resolve typescript build issue

--- packages/cloudflare-tools/src/utils/http-handler.ts
+++ packages/cloudflare-tools/src/utils/http-handler.ts
@@ -1,11 +1,12 @@
 import { NodeHttp2Handler } from "@smithy/node-http-handler";
+import { HttpHandler } from "@smithy/protocol-http";
 
 /**
  * Create a HTTP handler with a 5-minute timeout
  *
  * @returns A NodeHttp2Handler instance with custom request and session timeouts
  */
-export function createHttpHandler() {
+export function createHttpHandler(): HttpHandler<any> | NodeHttp2Handler {
   return NodeHttp2Handler.create({
     requestTimeout: 20 * 60 * 1000, // 20 minutes
     sessionTimeout: 30 * 60 * 1000, // 30 minutes
