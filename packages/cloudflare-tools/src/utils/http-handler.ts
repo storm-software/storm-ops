6fd02ad1d5838301f1883c58860975a88700c770
Pat <pat@patsullivan.org>
2025-12-17 03:32:15 +0000

    fix(cloudflare-tools): Update to http handler from http2

--- packages/cloudflare-tools/src/utils/http-handler.ts
+++ packages/cloudflare-tools/src/utils/http-handler.ts
@@ -1,4 +1,4 @@
-import { NodeHttp2Handler } from "@smithy/node-http-handler";
+import { NodeHttpHandler } from "@smithy/node-http-handler";
 import { HttpHandler } from "@smithy/protocol-http";
 
 /**
@@ -6,10 +6,9 @@ import { HttpHandler } from "@smithy/protocol-http";
  *
  * @returns A NodeHttp2Handler instance with custom request and session timeouts
  */
-export function createHttpHandler(): HttpHandler<any> | NodeHttp2Handler {
-  return NodeHttp2Handler.create({
-    requestTimeout: 20 * 60 * 1000, // 20 minutes
-    sessionTimeout: 30 * 60 * 1000, // 30 minutes
-    maxConcurrentStreams: 100
+export function createHttpHandler(): HttpHandler<any> | NodeHttpHandler {
+  return NodeHttpHandler.create({
+    connectionTimeout: 30 * 60 * 1000, // 30 minutes
+    requestTimeout: 30 * 60 * 1000 // 30 minutes
   });
 }
