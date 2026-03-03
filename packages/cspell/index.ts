7abd498bde34799200a608aafe785ff5b08b2a1b
Pat <pat@patsullivan.org>
2026-03-03 23:12:07 +0000

    feat(cspell): Cleaned up the CSpell shared configurations

--- packages/cspell/index.ts
+++ packages/cspell/index.ts
@@ -1,13 +1,13 @@
 import cspellAll from "./cspell-all.json";
-import cspellBase from "./cspell-base.json";
 import cspellExt from "./cspell-ext.json";
+import cspellExtra from "./cspell-extra.json";
 import cspellMinimal from "./cspell-minimal.json";
 import cspellRecommended from "./cspell-recommended.json";
 
 export const configs = {
-  base: cspellBase,
+  ext: cspellExt,
   minimal: cspellMinimal,
   recommended: cspellRecommended,
-  ext: cspellExt,
+  extra: cspellExtra,
   all: cspellAll
 };
