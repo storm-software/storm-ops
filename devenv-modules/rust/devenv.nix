47a91163c9ca2ee64a65f81886e82c227258423f
Pat <pat@patsullivan.org>
2026-03-19 05:32:11 +0000

    feat(git-tools): Added `lint` project target to `lefthook` config

--- devenv-modules/rust/devenv.nix
+++ devenv-modules/rust/devenv.nix
@@ -7,21 +7,9 @@
     pkgs.capnproto-rust
   ];
 
-  # https://devenv.sh/languages/
   languages.rust = {
     enable = true;
   };
 
-  # https://devenv.sh/processes/
   processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/git-hooks/
-  git-hooks = {
-    enable = true;
-    hooks = {
-      cargo-check.enable = true;
-      rustfmt.enable = true;
-      clippy.enable = true;
-    };
-  };
 }
