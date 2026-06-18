7ccbee070a652bc57d612efd53724cdb52657ac1
sullivanpj <pat@patsullivan.org>
2026-06-18 09:16:38 +0000

    fix(eslint): Remove unneeded tsdoc dependency for eslint config

--- packages/eslint/src/configs/tsdoc.ts
+++ packages/eslint/src/configs/tsdoc.ts
@@ -1,7 +1,7 @@
-import pluginTsdoc from "@storm-software/eslint-plugin-tsdoc";
 import { GLOB_TS, GLOB_TSX } from "@storm-software/package-constants/globs";
 import type { OptionsFiles, OptionsTSDoc, TypedFlatConfigItem } from "../types";
 import { findWorkspaceRoot } from "../utils/find-workspace-root";
+import { ensurePackages, interopDefault } from "../utils/helpers";
 
 export async function tsdoc(
   options: OptionsFiles & OptionsTSDoc = {}
@@ -12,6 +12,12 @@ export async function tsdoc(
     configFile,
     files = [GLOB_TS, GLOB_TSX]
   } = options;
+
+  await ensurePackages(["@storm-software/eslint-plugin-tsdoc"]);
+
+  const [pluginTsdoc] = await Promise.all([
+    interopDefault(import("@storm-software/eslint-plugin-tsdoc"))
+  ] as const);
 
   return [
     {
