1a543f7f53f1cfb34a7248720c8b1067f52e1cf1
sullivanpj <pat@patsullivan.org>
2026-07-16 05:08:26 +0000

    chore(git-tools): Added nested `bun` and `pnpm` modules for `lefthook`

--- packages/eslint/src/typegen.d.ts
+++ packages/eslint/src/typegen.d.ts
@@ -6609,6 +6609,7 @@ Backward pagination arguments
   /**
    * Disallow function declarations that contain unsafe references inside loop statements
    * @see https://typescript-eslint.io/rules/no-loop-func
+   * @deprecated
    */
   'ts/no-loop-func'?: Linter.RuleEntry<[]>
   /**
